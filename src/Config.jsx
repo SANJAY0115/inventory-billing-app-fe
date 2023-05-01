a3cc18df0eb2ac57b3ab4ee77c35dd6df1a1bf5f
SANJAY0115 <sanjaysanju0115@gmail.com>
2023-05-01 11:57:46 +0530

    backend deployed url added

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,6 +1,9 @@
 export const Config = {
     // api:"http://localhost:4000"
-    api:"http://localhost:1009"
+    // api:"http://localhost:1009"
+    
+    api: "inventory-billing-app-be.vercel.app"
+
    // api:"https://inventory-billing-kli1.onrender.com"
 }
 
